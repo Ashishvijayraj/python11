6b422625d046fbcaa2259f748ed4e118ee462b7e
Ashish Raj <77605211+ashr9674@users.noreply.github.com>
2022-02-02 12:08:43 +0530

    Add files via upload
    Program of to find Addition, Substraction, Multiplication, Division of Three Numbers

---
+++ Python11.py
@@ -0,0 +1,7 @@
+a1=int(input("Enter the first number \n"))
+a2=int(input("Enter the second number \n"))
+a3=int(input("Enter the third number \n"))
+print("Addition of three Numbers", int(a1)+int(a2)+int(a3))
+print("Multiplication of three Numbers", int(a1)*int(a2)*int(a3))
+print("Division of three Numbers", int(a1)/int(a2)/int(a3))
+print("Substraction of  three Numbers", int(a1)-int(a2)-int(a3))
