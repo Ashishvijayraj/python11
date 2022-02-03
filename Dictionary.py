ad16282dfea3881e097b9e8fa7e364cf0ae11823
Ashish Raj <77605211+ashr9674@users.noreply.github.com>
2022-02-03 13:51:39 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+w1={"My name is":"Ashish","Branch":"CSE","Year":"2022","Favorite_Movie":"John Wick","Favorite_Web series":"Game of Thrones"}
+print(w1)
+print(w1["Branch"])
+print(w1["Year"])
+print(w1["My name is"])
+print(w1["Favorite_Web series"])
+print(w1["Favorite_Movie"])
+
+
---
+++ String Slicing.py
@@ -0,0 +1,8 @@
+# slicing is the concept in which given data is divided and also we can find, what element is on that on particular place
+
+m1="Computer Science and Engineering"
+print(m1[0:7])
+print(m1[3:5])
+print(m1[::-1])
+print(m1[2:-3])
+print(m1[0:5:2])
