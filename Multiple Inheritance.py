92adbd86a915149ec1ccb87c3adf09b3acdf31fd
Ashish Raj <77605211+ashr9674@users.noreply.github.com>
2022-03-15 00:35:22 +0530

    Add files via upload

---
+++ Multiple Inheritance.py
@@ -0,0 +1,13 @@
+class mike:
+    dance = 877
+    chicken = 8
+class shayam(mike):
+    dance = 769548
+    tube = "lllll"
+class vikram(mike):
+    dance = 99999
+    chicken = 8888
+tony = mike()
+sam = shayam()
+don = vikram()
+print(sam.dance)
